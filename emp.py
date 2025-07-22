e460136dd3b082b170d6af9000bf505f0685441f
Harsha H <harsha2041889@gmail.com>
2025-07-22 20:28:00 +0530

    UseCase2_Calculate_Daily_Wage

--- emp.py
+++ emp.py
@@ -1,3 +1,4 @@
+
 import random
 
 def is_present()->int:
@@ -11,14 +12,62 @@ def is_present()->int:
         It returns a int value 0 or 1 based on random.randint(0,1)
         If the return value is 1 than it is considered as present else absent.
     """
-    
     return random.randint(0,1)
 
+# try:
+#     if is_present() == 1 :
+#         print("Employee is present today")
+#     else:
+#         print("Employee is absent today")
+        
+# except Exception as e:
+#     print(f"Exception occured {e} please try again")
+
+
+def calc_daily_wage(wage_per_hr, hr_per_day):
+    """
+        Description:
+        This function is used to calculate daily wage of an employee
+
+        Parameter:
+        wage_per_hr : It is employee's per hour wage
+        hr_per_day : It is employee's per day working hours        
+
+        Return:
+        It returns daily wage of employee as integer value
+    """   
+    daily_wage = hr_per_day * wage_per_hr
+    return daily_wage
+
+def part_or_full():
+    """
+        Description:
+        This function is know whether the employee is part time or full time
+
+        Parameter:
+        None
+
+        Return:
+        It returns an Integer value 1 or 2, for part time or full time
+    """
+    return random.randint(1,2)   
+
 try:
-    if is_present() == 1 :
+    if is_present() :
         print("Employee is present today")
+        match part_or_full():
+            case 1:
+                #When employee is part time
+                daily_wage = calc_daily_wage(wage_per_hr=20, hr_per_day=4)
+                wage_type = "Part time"
+            case 2:
+                #When employee is full time
+                daily_wage = calc_daily_wage(wage_per_hr=20, hr_per_day=8)
+                wage_type = "Full time"
+        print(f"Employee's daily wage is {daily_wage} as employee is {wage_type}")
     else:
         print("Employee is absent today")
         
 except Exception as e:
     print(f"Exception occured {e} please try again")
+     
