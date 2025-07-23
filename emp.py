e2b2c98223032038a39011cc9477817c681da0f4
Harsha H <harsha2041889@gmail.com>
2025-07-23 11:35:12 +0530

    Initial:commit

--- emp.py
+++ emp.py
@@ -46,12 +46,16 @@ def part_or_full():
 try:
     if is_present() :
         print("Employee is present today")
-        case = part_or_full()
-        if case ==1:
-            #When employee is part time
-            daily_wage = calc_daily_wage(wage_per_hr=20, hr_per_day=4)
-            wage_type = "Part time"
-        else:
+        """
+        We are using the match since instead of  switch
+        Since there is switch case in python
+        """
+        match part_or_full():
+            case 1:
+                #When employee is part time
+                daily_wage = calc_daily_wage(wage_per_hr=20, hr_per_day=4)
+                wage_type = "Part time"
+            case 2:
                 #When employee is full time
                 daily_wage = calc_daily_wage(wage_per_hr=20, hr_per_day=8)
                 wage_type = "Full time"
